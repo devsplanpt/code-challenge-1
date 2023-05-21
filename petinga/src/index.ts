5358cd61651e0da4343d5d453054765c7835b2dd
Pedro Cruz <pedropcruz@outlook.pt>
2023-05-21 16:02:43 +0100

    feat(): Add unit tests to petinga folder

--- petinga/src/index.ts
+++ petinga/src/index.ts
@@ -1,3 +1,10 @@
-const a: string = 'HELLO';
-
-console.log(a);
+/**
+ * Returns the input string if it is truthy, otherwise returns false.
+ *
+ * @param {string | undefined} str - The input string to be tested.
+ * @returns {string | boolean} - If the input string is truthy, it is returned. Otherwise, false is returned.
+ */
+export function test(str?: string): string | boolean {
+	if (!str) return false;
+	return str;
+}
